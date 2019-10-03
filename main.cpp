2673e8399fb72589c6a56c5b2a7dae5ce82ed592
Echo <liuy218@wfu.edu>
2019-10-03 15:04:43 -0400

    Echo's Project 3

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 #include <ctime>
 #include <cassert>
 using namespace std;
+/* Name: Echo Liu
+ * Project: project 3:creating bubblesort
+ * Time: Oct.3rd.2019
+ * Class: CSC 112
+ */
 
 // MergeSort function declarations
 void mergeSort(vector<int>& a, vector<int>& tmp, int left, int right);
@@ -12,16 +17,28 @@ void mergeSortedLists(vector<int>& a, vector<int>& tmp, int leftPos, int rightPo
 void swap (int *a, int *b);
 void bubbleSort(int *a, int n);
 
-int main() {
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {//because we enter two numbers so that our argc can not be longer than 3
+        cout << "Limit to enter two numbers. One for seed and one for size"  << endl;
+        cout << endl;
+        exit(1);
+    }
+
     // get input: first is random seed, second is vector length
     int seed, length;
-    cin >> seed >> length;
+    seed = atoi(argv[1]);//seed is on the second position of the thing we enter;
+    length = atoi(argv[2]);//length is on the third place;
+    cout << "Please enter seed and length in terminal:"<< seed << length<< endl;
     srand(seed);
 
     vector<int> v(length); // vector to be sorted
     vector<int> t(length); // temporary workspace
 
     // define an integer pointer dynamically allocate an array of integers
+    int * a = new int[length];
+    int * x = a;
     /* your code here */
 
     // initialize and print input
@@ -33,7 +50,11 @@ int main() {
     cout << endl;
 
     // copy the random list of integers from vector to array
-    /* your code here */
+
+    // int a new array;
+    for(int i=0;i<length;i++){
+        a[i]=v.at(i);//we change the vector into array
+    }/* your code here */
 
     clock_t start_mergeSort = clock();
     // sort vector using mergeSort
@@ -45,13 +66,20 @@ int main() {
         assert(v.at(i-1) <= v.at(i));
     }
 
-    // check output, make sure array is sorted after bubbleSort
-    /* your code here */
 
     clock_t start_bubbleSort = clock();
     // sort array using bubbleSort
+    bubbleSort(x,length);//call the bubbleSort here to access the the bubble sort array
     /* your code here */
+
+
     clock_t end_bubbleSort = clock();
+
+    // check output, make sure array is sorted after bubbleSort
+    for(int i = 1; i < v.size(); i++) {
+        assert(a[i-1] <= a[i]);
+    }
+    /* your code here */
 
 
     // print sorted vector after mergeSort
@@ -62,13 +90,68 @@ int main() {
     cout << endl;
 
     // print sorted array after bubbleSort
-    /* your code here */
+
+    cout << "Bubble Sorted: "<<endl ;
+    for(int i = 0 ;i <v.size();i++){
+        cout<< a[i]<<'\t';
+    }
+    cout<<endl;
 
     // print elapsed time for mergeSort and bubbleSort
     double elapsed_mergeSort = double(end_mergeSort - start_mergeSort) / CLOCKS_PER_SEC;
     double elapsed_bubbleSort = double(end_bubbleSort - start_bubbleSort) / CLOCKS_PER_SEC;
 
-    cout << elapsed_mergeSort << " " << elapsed_bubbleSort << endl;
+    cout << " * Elapsed time for sorting a " << length << "-element array: " << endl;
+    cout << "  MergeSort: " << elapsed_mergeSort << endl;
+    cout << "  BubbleSort: " << elapsed_bubbleSort << endl;
+
+
+
+    // unit test for merge
+    // print out messages for the comparsion between
+    // the original unit test vector's expected sorted vector and the actual one
+    cout << "Swap's Unit test" << endl;
+    int unitesArr[] = {1, 100};
+    int unitesExpectedArr[] = {1000, 1};
+    swap(unitesArr[0], unitesArr[1]);
+
+    cout << " Expect array {1, 100} to get {100, 1}" << endl;
+    cout << " In fact, we get {";
+    for(int i = 0; i < 2; i++) {
+        if (i == 0){
+            cout << unitesArr[i] << ", ";
+        }
+        else{
+            cout << unitesArr[i] << "}" << endl;
+        }
+    }
+    for(int i = 0; i < 2; i++) {
+        assert(unitesArr[i] == unitesExpectedArr[i]);
+    }
+
+    //do the bubblesort unit test
+    cout << "BubbleSort test" << endl;
+    int testArray[] = {1, 2,7,3 , 4 ,6,5};
+    int unitExpected[] = {1, 2, 3, 4, 5, 6, 7};
+
+    cout << "Expect array{1, 2,7,3,4,6,5}; to get  {1, 2, 3, 4, 5, 6, 7}" << endl;
+    cout << "In fact, we get:{";
+    bubbleSort(testArray, 7);
+    // print the sorted vector by using a for loop
+    for(int i = 0; i < 7; i++) {
+        if (i != 6){
+            cout << testArray[i] << ", ";
+        }
+        else{
+            cout << testArray[i] << "}" << endl << endl;
+        }
+    }
+
+    for(int i = 0; i < 7; i++) {
+        assert(testArray[i] == unitExpected[i]);
+    }
+
+
 
     return 0;
 }
@@ -109,11 +192,21 @@ void mergeSort(vector<int>& a, vector<int>& tmp, int left, int right) {
 }
 
 // Swap function
-void swap(int *a, int *b) {
-    /* your code here */
+void swap(int *a, int *b) {//defining swap function which is used in the bubbleSort
+    int *temp;
+    *temp = *a;
+    *a = *b;
+    *b = *temp;/* your code here */
 }
 
 // BubbleSort function
 void bubbleSort(int *a, int n) {
     /* your code here */
-}
+    for (int i = 0; i < n - 1; i++) {//using for loop to make out the bubble sort list
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+}/* your code here */
